145508a21ee047a047a89f0ddfbcfe4808121885
Artashes <da@burno.tech>
2022-09-30 18:32:33 +0300

    1

--- resources/shared/components/Field/Field.vue
+++ resources/shared/components/Field/Field.vue
@@ -4,7 +4,9 @@
     )
         .__cell(
             v-for="item in cellContent"
-        ) {{item.id}}
+            :style="{background: item.select ? 'blue' : 'white'}"
+            v-on:click="select(item.id)"
+        ) {{item.value}}
 </template>
 
 <script lang="ts">
@@ -20,18 +22,27 @@ export default class Field extends Vue {
     @Prop({default: 10}) x!: FieldProps['xCount']
     @Prop({default: 5}) y!: FieldProps['yCount']
 
+    values = ['1', '2', '3', '4']
     fieldStyle = {
         maxWidth: `${this.x! * 40}px`,
     }
+    content = []
 
-    get cellContent() {
-        let array = new Array(this.x! * this.y!).fill({id: 1})
+    getCellContent() {
+        this.content = new Array(this.x! * this.y!).fill({id: 1})
+            .map((item, index) => ({
+                id: index + 1,
+                value: this.values[this.generateRandomValue(this.values)],
+                select: false
+            }))
+    }
 
-        array = array.map((item, index) => ({
-            id: index + 1,
-        }))
+    generateRandomValue(array: string[]) {
+        return Math.floor(Math.random() * array.length)
+    }
 
-        return array
+    mounted() {
+        this.getCellContent()
     }
 
 }
